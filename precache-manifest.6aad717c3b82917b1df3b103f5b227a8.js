70dac7c8df55d45ec8d1cc14ec715e06b3ed8919
Ruben Ramirez <ruben1991@protonmail.com>
2020-10-02 23:08:17 -0700

    Updates

---
+++ precache-manifest.6aad717c3b82917b1df3b103f5b227a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a7557f9266f550b82c7200e8f91088",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7b7dbb3cfe4772f806bf",
+    "url": "/portfolio/static/css/main.9b711215.chunk.css"
+  },
+  {
+    "revision": "7b3e32a83613faeb64bc",
+    "url": "/portfolio/static/js/2.92888f61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.92888f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7dbb3cfe4772f806bf",
+    "url": "/portfolio/static/js/main.77438e0b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c63c1324c3482319f54914a424417719",
+    "url": "/portfolio/static/media/code.c63c1324.svg"
+  },
+  {
+    "revision": "81eda716e82c506fbc256d278808a855",
+    "url": "/portfolio/static/media/csharp.81eda716.png"
+  },
+  {
+    "revision": "ab7da72ab46e1af73d2d425164b0e1ef",
+    "url": "/portfolio/static/media/css.ab7da72a.png"
+  },
+  {
+    "revision": "2d78f49311cf0c079ea8aa1cae1a9de4",
+    "url": "/portfolio/static/media/git.2d78f493.png"
+  },
+  {
+    "revision": "6cf2d330c620c4c0ea001e4e39e4c843",
+    "url": "/portfolio/static/media/java.6cf2d330.png"
+  },
+  {
+    "revision": "15a4866e67b5a9aac43ae3af08de6721",
+    "url": "/portfolio/static/media/javascript.15a4866e.png"
+  },
+  {
+    "revision": "fd2220dbb5a9c400d06b771a0a6b01ed",
+    "url": "/portfolio/static/media/linux.fd2220db.png"
+  },
+  {
+    "revision": "6047913cbe97b94d103cf3e960de68eb",
+    "url": "/portfolio/static/media/net.6047913c.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/portfolio/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "93e8352c1cf576b59267860988a7a2cc",
+    "url": "/portfolio/static/media/resume.93e8352c.pdf"
+  },
+  {
+    "revision": "37f5f6bf307cebd622457caa251026b3",
+    "url": "/portfolio/static/media/ruben.37f5f6bf.png"
+  },
+  {
+    "revision": "597cd41b2d741adf3eff7d5a1d695cc1",
+    "url": "/portfolio/static/media/sql.597cd41b.png"
+  },
+  {
+    "revision": "9a0683e93da9a57d3b1dd0f211f89505",
+    "url": "/portfolio/static/media/typescript.9a0683e9.png"
+  }
+]);
