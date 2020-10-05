a8151bc6a599abe3d5622df6926273e015bf5006
Ruben Ramirez <ruben1991@protonmail.com>
2020-10-05 12:54:45 -0700

    Added text typing animations

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <section>
       <div className="hero col wrapper">
         <p className="name">Ruben Ramirez</p>
-        <h1>Software Developer</h1>
+        <h1 className="title">Software Developer</h1>
         <img src={headshot} className="headshot" alt="Dev"/>
         <p className="desc"> I'm a full stack developer working in San Diego. I have a passion for front end development and UI design. I have worked on developing multiple commercial scale web applications with Angular and .Net Core. Want to work together? </p>
         <a href={resume} target='_blank'rel="noopener noreferrer" className="cta">Resume</a> 
