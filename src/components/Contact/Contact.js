0627133f1f160f5c4c0712a87e2aa8f195dbefa1
Ruben Ramirez <ruben1991@protonmail.com>
2020-10-05 21:34:33 -0700

    CSS cleanup

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Contact.scoped.css';
 
 const Contact = (props) => {
+  const email = 'ruben1991@protonmail.com';
+
   return (
     <section className="contact" ref={props.elementRef}>
       <h2 className="title">Interested in working together?</h2>
-      <a href="mailto:ruben1991@protonmail.com" className="email-btn">Email Me</a>
+      <a href={`mailto:${email}`} className="email-btn">Email Me</a>
     </section>
   )
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,9 @@ const Home = () => {
         <p className="name">Ruben Ramirez</p>
         <h1 className="title">Software Developer</h1>
         <img src={headshot} className="headshot" alt="Dev"/>
-        <p className="desc"> I'm a full stack developer working in San Diego. I have a passion for front end development and UI design. I have worked on developing multiple commercial scale web applications with Angular and .Net Core. Want to work together? </p>
+        <p className="desc">
+          I'm a full stack developer working in San Diego. I have a passion for front end development and UI design. I have worked on developing multiple commercial scale web applications with Angular and .Net Core. Want to work together? 
+        </p>
         <a href={resume} target='_blank'rel="noopener noreferrer" className="cta">Resume</a> 
       </div>
     </section>
