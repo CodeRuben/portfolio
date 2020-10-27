965ae7e4462a4192c7d66698d3330f731298cdd6
Ruben Ramirez <ruben1991@protonmail.com>
2020-10-27 12:50:25 -0700

    Updated alt tags

--- src/components/SkillCard/SkillCard.js
+++ src/components/SkillCard/SkillCard.js
@@ -5,13 +5,13 @@ const SkillInfo = (props) => {
   const data = props.data;
   return (
     <div className="card col">
-      <img src={data.icon} alt="" className="dev-icon" />
+      <img src={data.icon} alt="developer icon" className="dev-icon" />
       <h3>{data.title}</h3>
       <p className="desc">{data.description}</p>
       <div className="skill-icons">
         {data.skills.map((skill, index) => 
           <div className="icon" style={{background: skill.background}} key={index}>
-            <img src={skill.icon} alt="angular" className="skill-icon" />
+            <img src={skill.icon} alt="skill" className="skill-icon" />
           </div>
         )}
       </div>
