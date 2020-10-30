83674a74eeec41da3bc51052209c861f495b81a7
Ruben Ramirez <ruben1991@protonmail.com>
2020-10-30 12:42:34 -0700

    Created project section. Added project card component to display project data

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ProjectCard.scoped.css';
+import tracer from '../../assets/tracer.svg';
+import range from '../../assets/range.svg';
+
+const ProjectCard = () => {
+  return (
+    <div className="project-card">
+      <div className="content">
+        <img src={range} alt="first" className="screen-one" />
+      </div>
+      <h3>Range Explorer</h3>
+      <p className="sub-title">Poker training application</p>
+      <p className="description">Application allows users to view preflop ranges and simulate heads up scenarios. Users also log and review session results.</p>
+    </div>
+  );
+} 
+
+export default ProjectCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Projects.scoped.css';
+import ProjectCard from '../ProjectCard/ProjectCard';
+
+const Projects = (props) => {
+  return (
+    <div className="container" ref={props.elementRef}>
+      <div className="wrapper">
+        <h2 className="title">Recent Work</h2>
+        <div className="-top-decorator"></div>
+        <div className="center">
+          <ProjectCard /> 
+        </div>
+        <div className="-bottom-decorator"></div>
+      </div>
+    </div>
+  )
+};
+
+export default Projects;
