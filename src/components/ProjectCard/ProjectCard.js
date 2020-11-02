c5b4433080dcfc0705981ba3cdf72c35413cdc32
Ruben Ramirez <ruben1991@protonmail.com>
2020-11-01 19:05:55 -0800

    Added arrow buttons to project card to cycle through screenshots

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProjectCard.scoped.css';
+import rightArrow from '../../assets/right-arrow.svg';
+import leftArrow from '../../assets/left-arrow.svg';
+
+const ArrowButton = React.memo((props) => {
+  return (
+    <span onClick={() => props.click()} className="arrow-container">
+      <img src={props.imgSrc} alt="arrow" className="next-arrow" />
+    </span>
+  )
+})
 
 const ProjectCard = (props) => {
+  const [imageIndex, updateImageIndex] = useState(0);
+
+  const updateSelectedImage = () => {
+    let newIndex = imageIndex === 0 ? 1 : 0;
+    updateImageIndex(newIndex);
+  }
+
   return (
     <div className="project-card">
       <div className="content">
-        <img src={props.screenshots[0]} alt="app-screenshot" className="screenshot" />
+        <ArrowButton imgSrc={leftArrow} click={updateSelectedImage} />
+        <img src={props.screenshots[imageIndex]} alt="app-screenshot" className="screenshot" />
+        <ArrowButton imgSrc={rightArrow} click={updateSelectedImage} />
       </div>
       <h3>{props.title}</h3>
       <p className="sub-title">{props.subTitle}</p>
